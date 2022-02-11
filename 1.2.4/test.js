89564d7a58188db3cb7853c04153e79095f46b69
EvilCoreS <39567590+EvilCoreS@users.noreply.github.com>
2022-02-11 17:16:40 +0200

    initial commit

--- 1.2.4/test.js
+++
@@ -1,9 +0,0 @@
-let string = "login=student&password=12345"
-let buff
-try {
-    buff = require("fs").readFileSync("passswords.txt")
-    console.log(buff.toString())
-}
-catch {
-    console.log("error")
-}
